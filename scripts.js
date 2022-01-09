b3fc0fab4eeeaee36af0de4bb4f1c6f10cb26178
francienevaz <fran.vfraga@gmail.com>
2022-01-09 20:23:26 -0300

    update

--- scripts.js
+++ scripts.js
@@ -91,9 +91,13 @@ let playGame = () => {
     nextLevel();
 }
 
-green.addEventListener('click', click(0));
-red.addEventListener('click', click(1));
-yellow.addEventListener('click', click(2));
-blue.addEventListener('click', click(3));
+
+//eventos click para as cores
+green.onclick = () => click(0);
+red.onclick = () => click(1);
+yellow.onclick = () => click(2);
+blue.onclick = () => click(3);
+
+
 
 playGame();
